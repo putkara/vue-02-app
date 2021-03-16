48fe8d54459e59a847cbe11d7b134331f634caa3
Natia Meskhi <natia.meskhi.natia@gmail.com>
2021-03-16 15:23:43 +0100

    task-tracker-app

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+<h1>Boom</h1>
+ 
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+
+
+@Options({
+  components: {
+   
+  },
+})
+export default class App extends Vue {}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
