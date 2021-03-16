e1ff146693253c0f861a8fb482a184ef71b35215
Natia Meskhi <natia.meskhi.natia@gmail.com>
2021-03-16 16:36:53 +0100

    Header Component

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,65 @@
 <template>
-<h1>Boom</h1>
+<div class="container">
+  <Header title="Task Tracer"/>
+</div>
+
  
 </template>
 
-<script lang="ts">
-import { Options, Vue } from 'vue-class-component';
+<script >
+
+import Header from './components/Header'
+export default {
+  name:'App',
+  components:{
+    Header
+  }
+}
 
 
-@Options({
-  components: {
-   
-  },
-})
-export default class App extends Vue {}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400&display=swap');
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+body {
+  font-family: 'Poppins', sans-serif;
+}
+.container {
+  max-width: 500px;
+  margin: 30px auto;
+  overflow: auto;
+  min-height: 300px;
+  border: 1px solid steelblue;
+  padding: 30px;
+  border-radius: 5px;
+}
+.btn {
+  display: inline-block;
+  background: #000;
+  color: #fff;
+  border: none;
+  padding: 10px 20px;
+  margin: 5px;
+  border-radius: 5px;
+  cursor: pointer;
+  text-decoration: none;
+  font-size: 15px;
+  font-family: inherit;
+}
+.btn:focus {
+  outline: none;
+}
+.btn:active {
+  transform: scale(0.98);
+}
+.btn-block {
+  display: block;
+  width: 100%;
 }
 </style>
+
